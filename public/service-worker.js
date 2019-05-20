0f250c3398151511c1ac0f10e01e6b6f62a6edbf
root <x250>
2019-05-20 11:57:26 +0800

    add workerbox

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,13 +16,25 @@
  * limitations under the License
  */
 'use strict';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+workbox.googleAnalytics.initialize();
 
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v1';
 
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-];
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+workbox.setConfig({ debug: true });
+
+workbox.core.setCacheNameDetails({
+  prefix: 'my-app',
+  suffix: 'v1',
+  precache: 'custom-precache-name',
+  runtime: 'custom-runtime-name'
+});
+
+
 
 self.addEventListener('install', (evt) => {
   console.log('[ServiceWorker] Install');
@@ -43,3 +55,40 @@ self.addEventListener('fetch', (evt) => {
   // CODELAB: Add fetch event handler here.
 
 });
+
+
+workbox.routing.registerRoute(
+  new RegExp('.*\.js'),
+  new workbox.strategies.NetworkFirst()
+);
+workbox.routing.registerRoute(
+  // Cache CSS files.
+  /\.css$/,
+  // Use cache but update in the background.
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
+workbox.routing.registerRoute(
+  // Cache image files.
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  // Use the cache if it's available.
+  new workbox.strategies.CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+workbox.routing.registerRoute(
+  /index\.html/,
+  new workbox.strategies.StaleWhileRevalidate()
+);
+
