2af79fd644981bfab66f2f7b4ba5bd668cba9df1
root <x250>
2019-05-21 19:26:18 +0800

    word story

---
+++ public/modules/common/common.js
@@ -0,0 +1,33 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-101623151-11');
+function ga(){
+    console.debug("ga", Array.prototype.slice.call(arguments).join(","));
+    /**
+     gtag('event', <action>, {
+              'event_category': <category>,
+              'event_label': <label>,
+              'value': <value>
+            });
+     */
+}
+gtag('event', 'screen_view', {
+    'app_name': 'WordConnect',
+    'screen_name' : 'Home'
+});
+setInterval(function(){
+    gtag('event', 'auto_engagement', {
+        non_interaction: true
+    });
+}, 10000);
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
