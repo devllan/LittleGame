720097785a3ad61851d8ed3271a8f86ff88cf287
root <x250>
2019-06-19 20:57:30 +0800

    add 2048

--- public/modules/common/common.js
+++ public/modules/common/common.js
@@ -1,4 +1,8 @@
 window.dataLayer = window.dataLayer || [];
+// (function(C,l,a,y,_,i,o){C[_]=C[_]||function(){
+//     (C[_].q=C[_].q||[]).push(arguments)},C[_].l=1*new Date();i=l.createElement(a),
+//     o=l.getElementsByTagName(a)[0];i.async=1;i.src=y;o.parentNode.insertBefore(i,o)
+// })(window,document,'script',"",'gtag');
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
---
+++ public/modules/common/common_v2.js
@@ -0,0 +1,29 @@
+function initGTag(){
+    var e = document.createElement("script");
+    e.async = 1;e.src = "https://www.googletagmanager.com/gtag/js?id=UA-101623151-11";
+    var o = document.getElementsByTagName("script")[0];
+    o.parentNode.insertBefore(e, o);
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function(){dataLayer.push(arguments);};
+    window.ga = console.log;
+    gtag('js', new Date());
+    gtag('config', 'UA-101623151-11');
+    gtag('event', 'screen_view');
+
+}
+initGTag();
+setInterval(function(){
+    gtag('event', 'auto_engagement', {
+        non_interaction: true
+    });
+}, 30000);
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
