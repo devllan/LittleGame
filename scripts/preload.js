c3ca2f9f03026d8519f19e2984f464d3c4c01e8b
wujiankun <807648615@qq.com>
2019-07-09 13:03:40 +0800

    k Resource loading

--- scripts/preload.js
+++ scripts/preload.js
@@ -1,19 +1,62 @@
 const fs = require("fs");
 const path = require("path");
 const ModulesPath = path.join(__dirname, "../public/modules");
-const ModulesOrder = [
+let ModulesOrder = [
 
 ];
+/**
+ * let content = fs.readFileSync();
+ * content.replace(/RESOURCES\s*=\s*\[[\s\S]+\]/, function(){
+ *  return "RESOURCES = [" + files.join(",") + "]";
+ * })
+ * 
+ * 
+ */
+const dirs = fs.readdirSync(ModulesPath, {
+    encoding: "utf8"
+});
+var zz = /(.mp3)$/;
 
-const dirs = fs.readdirSync(ModulePath, {encoding: "utf8"});
-
-let files = [];
-function findAssets(dir){
-    const dirs = fs.readdirSync(dir, {encoding: "utf8"});
-    dirs.forEach(file =>{
-
+function travel(dir, callback) {
+    fs.readdirSync(dir).forEach(function (file) {
+        var pathname = path.join(dir, file);
+        for (j = 0, len = ModulesOrder.length; j < len; j++) {
+            if (ModulesOrder[j] == pathname) {
+                return
+            }
+        }
+        if (fs.statSync(pathname).isDirectory()) {
+            travel(pathname, callback);
+        } else {
+            callback(pathname);
+        }
     });
 }
-dirs.forEach( dir =>{
-    let rets = findAssets(path.join(ModulesPath, dir));
+travel(ModulesPath, function (pathname) {
+    //去掉MP3
+    if (zz.test(pathname)) {
+        console.log('mp3')
+    } else {
+        ModulesOrder.push(pathname);
+    }
 });
+//读取loader文件，进行改写。
+ModulesOrder = ModulesOrder.map(function (currentValue, index, arr) {
+    var _currentValue = currentValue;
+    var relative = path.relative(__dirname + "/../public", _currentValue);
+    // var n = relative.replace(/\\/g, "/")
+    return relative;
+    //换成相对路径
+})
+let content = fs.readFileSync(__dirname + "/../public/loader.html", {
+    encoding: "utf8"
+});
+content = content.replace(/RESOURCES\s*=\s*\[[\s\S]+\]/g, function () {
+    return "RESOURCES = " + JSON.stringify(ModulesOrder);
+});
+fs.writeFile('../public/loader.html', content, 'utf8', function (err) {
+    if (err) {
+        console.log(err)
+    };
+});
+console.log(content);
